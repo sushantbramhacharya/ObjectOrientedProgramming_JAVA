f673a6ea5ee5d95c1e716f8df507228b4d9651c7
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-09-01 17:21:58 +0545

    For Each loop in jva

---
+++ JAVA_1/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] names={"Kathmandu","Lalitpur","Bhaktapur","Kritipur","Kantipur"};
+        for(String name:names)
+        {
+            System.out.println(name);
+        }
+    }
+}
