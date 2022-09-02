4808a19f0725f5cff6a20d78a695e8cedcd696f4
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-09-02 13:49:35 +0545

    Classes and Object basics

---
+++ JAVA_2/ClassObject.java
@@ -0,0 +1,9 @@
+public class ClassObject {
+    int id=5;
+    String name ="Ram";
+    public static void main(String[] args) {
+        ClassObject object=new ClassObject();
+        System.out.println(object.id);
+        System.out.println(object.name);
+    }
+}
