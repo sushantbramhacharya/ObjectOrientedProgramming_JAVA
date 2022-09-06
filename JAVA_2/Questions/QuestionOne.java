092cd3658e9f1e8a2760043a80353d016d870a2c
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-09-06 12:07:28 +0545

    Normal Warnings FIxed

--- JAVA_2/Questions/QuestionOne.java
+++ JAVA_2/Questions/QuestionOne.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class QuestionOne {
     public static void main(String[] args) {
         System.out.println("Enter the numbers ");
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
         OddEven oe = new OddEven(num);
         oe.display();
     }
--- JAVA_2/Questions/QuestionThree.java
+++ JAVA_2/Questions/QuestionThree.java
@@ -6,15 +6,12 @@ public class QuestionThree {
         new Check(35);
     }
 }
-class Check{
-    public Check(int number)
-    {
-        if(number%5==0&&number%7!=0)
-        {
-            System.out.println("The number "+number+" is exactly divisible by 5 but not 7");
-        }
-        else
-        {
+
+class Check {
+    public Check(int number) {
+        if (number % 5 == 0 && number % 7 != 0) {
+            System.out.println("The number " + number + " is exactly divisible by 5 but not 7");
+        } else {
             System.out.println("It doesnt match condition");
         }
     }
