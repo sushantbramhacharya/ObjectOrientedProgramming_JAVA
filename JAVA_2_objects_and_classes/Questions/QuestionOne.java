c26d2203e4b972f070cbeb1f9b06a5f2bb85b99a
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-09-14 16:17:37 +0545

    Some MOdifications and Recovery

--- JAVA_2_objects_and_classes/Questions/QuestionOne.java
+++ JAVA_2_objects_and_classes/Questions/QuestionOne.java
@@ -1,14 +1,10 @@
 package Questions;
 
-import java.util.Scanner;
+
 
 public class QuestionOne {
     public static void main(String[] args) {
-        System.out.println("Enter the numbers ");
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        sc.close();
-        OddEven oe = new OddEven(num);
+        OddEven oe = new OddEven(10);
         oe.display();
     }
 }
