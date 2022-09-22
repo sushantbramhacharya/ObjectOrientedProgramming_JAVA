3198c6e42266c1d28bb6afe28a5898062b3aaab0
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-09-22 12:28:59 +0545

    Packages

---
+++ JAVA_6_Packages/package_1/PackageClass.java
@@ -0,0 +1,8 @@
+package package_1;
+import package_2.NewClass;
+public class PackageClass {
+    public static void main(String[] args) {
+        NewClass nc=new NewClass();
+        nc.Show();
+    }
+}
---
+++ JAVA_6_Packages/package_2/NewClass.java
@@ -0,0 +1,7 @@
+package package_2;
+public class NewClass{
+    public void Show()
+    {
+        System.out.println("Hello From New Class");
+    }
+}
